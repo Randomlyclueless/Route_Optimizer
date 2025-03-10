b25ca78868dbb38229c1994b114a85e3708dac3d
Randomlyclueless <144950366+Randomlyclueless@users.noreply.github.com>
2025-03-10 10:03:24 +0530

    .

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    app.config.from_object('app.config.Config')
+
+    db.init_app(app)
+    Migrate(app, db)
+
+    from app.routes.main import main_bp
+    from app.routes.api import api_bp
+    app.register_blueprint(main_bp)
+    app.register_blueprint(api_bp, url_prefix='/api')
